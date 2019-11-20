38c74abf78a9c887598abdd642b687141faf8a61
류태현 <dmgpgk1@naver.com>
2019-11-20 07:45:34 +0000

    뷰 실습 : 체크박스, 슬라이더
    git-tfs-id: [https://qhiana7.visualstudio.com/DefaultCollection]$/qhiana7/류태현/DPTIS_XamarinF_PR2;C1188

---
+++ DPTIS_XamarinF_PR2/DPTIS_XamarinF_PR2/Views/20190927/CheckBoxPR_Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DPTIS_XamarinF_PR2.Views._20190927
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CheckBoxPR_Page : ContentPage
+    {
+        public CheckBoxPR_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
